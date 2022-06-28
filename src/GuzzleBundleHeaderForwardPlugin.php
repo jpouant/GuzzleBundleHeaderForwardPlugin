df45f78ed50b74e087c93bc780081a34b03203b3
Justin Pouant <justin.pouant@ext.pichet.com>
2022-06-28 09:05:01 +0200

    fix interface and return types

--- src/GuzzleBundleHeaderForwardPlugin.php
+++ src/GuzzleBundleHeaderForwardPlugin.php
@@ -2,7 +2,7 @@
 
 namespace Neirda24\Bundle\GuzzleBundleHeaderForwardPlugin;
 
-use EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundlePlugin;
+use EightPoints\Bundle\GuzzleBundle\PluginInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class GuzzleBundleHeaderForwardPlugin extends Bundle implements EightPointsGuzzleBundlePlugin
+class GuzzleBundleHeaderForwardPlugin extends Bundle implements PluginInterface
 {
     /**
      * {@inheritdoc}
@@ -23,7 +23,7 @@ class GuzzleBundleHeaderForwardPlugin extends Bundle implements EightPointsGuzzl
     /**
      * {@inheritdoc}
      */
-    public function addConfiguration(ArrayNodeDefinition $pluginNode)
+    public function addConfiguration(ArrayNodeDefinition $pluginNode): void
     {
         $pluginNode
             ->canBeEnabled()
@@ -40,14 +40,14 @@ class GuzzleBundleHeaderForwardPlugin extends Bundle implements EightPointsGuzzl
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
     }
 
     /**
      * {@inheritdoc}
      */
-    public function loadForClient(array $config, ContainerBuilder $container, string $clientName, Definition $handler)
+    public function loadForClient(array $config, ContainerBuilder $container, string $clientName, Definition $handler): void
     {
         if (true === $config['enabled'] && !empty($config['headers'])) {
             $forwardHeaderMiddlewareDefinitionName = sprintf('guzzle_bundle_header_forward_plugin.middleware.%s', $clientName);
